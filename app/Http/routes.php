4232d3b2895d30dbbbc2807d72d304f2f94367cc
Muhammad Anis <anisiconic@gmail.com>
2019-03-20 14:38:55 +0700

    remove unused routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,21 +17,21 @@ Route::get('/', function () {
 
 Route::group(array('prefix' => 'api'), function() //, 'middleware' => 'auth0.jwt' , 'after' => 'add_token'
 {
-    Route::post('/hosts', ['as' => 'api', 'uses' => 'HostController@index']);
-    Route::post('/hosts/{id}', ['as' => 'api', 'uses' => 'HostController@show']);
+    // Route::post('/hosts', ['as' => 'api', 'uses' => 'HostController@index']);
+    // Route::post('/hosts/{id}', ['as' => 'api', 'uses' => 'HostController@show']);
 
 
-    Route::post('/stacks', ['as' => 'api', 'uses' => 'StackController@index']);
-    Route::post('/stacks/{id}', ['as' => 'api', 'uses' => 'StackController@show']);
-    Route::post('/stacks/{stack_id}/services', ['as' => 'api', 'uses' => 'ServiceController@index_by_stack']);
-    Route::post('/stacks/{stack_id}/services/{id}', ['as' => 'api', 'uses' => 'ServiceController@show_by_stack']);
+    // Route::post('/stacks', ['as' => 'api', 'uses' => 'StackController@index']);
+    // Route::post('/stacks/{id}', ['as' => 'api', 'uses' => 'StackController@show']);
+    // Route::post('/stacks/{stack_id}/services', ['as' => 'api', 'uses' => 'ServiceController@index_by_stack']);
+    // Route::post('/stacks/{stack_id}/services/{id}', ['as' => 'api', 'uses' => 'ServiceController@show_by_stack']);
 
 
-    Route::post('/services', ['as' => 'api', 'uses' => 'ServiceController@index']);
-    Route::post('/services/{id}', ['as' => 'api', 'uses' => 'ServiceController@show']);
+    // Route::post('/services', ['as' => 'api', 'uses' => 'ServiceController@index']);
+    // Route::post('/services/{id}', ['as' => 'api', 'uses' => 'ServiceController@show']);
     Route::post('/services/{id}/upgrade', ['as' => 'api', 'uses' => 'ServiceController@upgrade']);
-    Route::post('/services/{id}/finish_upgrade', ['as' => 'api', 'uses' => 'ServiceController@finish_upgrade']);
-    Route::post('/services/{id}/scale/{scale_count}', ['as' => 'api', 'uses' => 'ServiceController@scale']);
+    // Route::post('/services/{id}/finish_upgrade', ['as' => 'api', 'uses' => 'ServiceController@finish_upgrade']);
+    // Route::post('/services/{id}/scale/{scale_count}', ['as' => 'api', 'uses' => 'ServiceController@scale']);
 
 
 });
